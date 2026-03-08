af2f07eb8da642d14cb5caca3d0b680cb3df0ca2
indenterror20 <gaac.karlalden@ncba.edu.ph>
2026-03-08 20:45:22 +0800

    added error handling

---
+++ scripts/login.js
@@ -0,0 +1,31 @@
+//Inputs
+const emailInput = document.getElementById('formEmail');
+const passwordInput = document.getElementById('formPassword');
+//Error Handling
+const emailError = document.getElementById('emailError');
+const passwordError = document.getElementById('passwordError');
+
+//Empty Error Handling
+function validateEmail(){
+
+    if (emailInput.value === ''){
+    emailError.textContent = 'Please enter your email.'
+    return false;
+    }
+}
+
+function validatePassword(){
+    if (passwordInput.value === ''){
+    passwordError.textContent = 'Please enter your password.'
+    return false;
+    }
+}
+
+//Realtime validation
+emailInput.addEventListener('click', validateEmail);
+emailInput.addEventListener('blur', validateEmail);
+
+passwordInput.addEventListener('click', validatePassword);
+passwordInput.addEventListener('blur', validatePassword);
+
+
---
+++ scripts/signup.js
@@ -0,0 +1,54 @@
+//inputs
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+//error handling
+const usernameError = document.getElementById('usernameError');
+const emailError = document.getElementById('emailError');
+const passwordError = document.getElementById('passwordError');
+const confirmPassError = document.getElementById('confirmPassError');
+
+function validateUsername(){
+    if (username.value === ''){
+        usernameError.textContent = 'Please enter a username.';
+        return false;
+    }
+}
+
+function validateEmail(){
+    if (email.value === ''){
+        emailError.textContent = 'Please enter a email.';
+        return false;
+    }
+}
+
+function validatePass(){
+    if (password.value === ''){
+        passwordError.textContent = 'Please enter a password.';
+        return false;
+    }
+}
+
+function confirmPass(){
+    if (confirmPassword.value === ''){
+        confirmPassError.textContent = 'Please confirm your password.';
+    } else if (confirmPassword.value !== password.value){
+        confirmPassError.textContent = 'Passwords do not match.';
+    } else {
+        confirmPassError.textContent = ''; //clears error when password match
+    }
+}
+
+//realtime error handling
+username.addEventListener('click', validateUsername);
+username.addEventListener('blur', validateUsername);
+
+email.addEventListener('click', validateEmail);
+email.addEventListener('blur', validateEmail);
+
+password.addEventListener('click', validatePass);
+password.addEventListener('blur', validatePass);
+
+confirmPassword.addEventListener('click', confirmPass);
+confirmPassword.addEventListener('blur', confirmPass);
