5e50346faf5544a0184ec47d66395dfd75290db5
indenterror20 <gaac.karlalden@ncba.edu.ph>
2026-03-15 20:12:10 +0800

    setting up supabase

---
+++ node/supabase.js
@@ -0,0 +1,17 @@
+import {createClient}from '@supabase/supabase-js'
+import 'dotenv.config'
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY
+)
+
+const {data, error} = await supabase.from('users').select('*').limit(1);
+
+if(error){
+    console.log('database is not working');
+} else{
+    console.log('database connected');
+}
+
+export default supabase
